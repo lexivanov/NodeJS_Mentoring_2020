d5de9f3540b78282ae46b255ae45d9949e9f772e
Aleksandr Ivanov <Aleksandr_Ivanov4@epam.com>
2020-08-03 16:07:18 +0400

    [Task1]: moved task 1.2 to streams

--- Task1/task1.2.js
+++ Task1/task1.2.js
@@ -1,36 +1,31 @@
-const readline = require('readline');
 const fs = require("fs");
 const csv = require("csvtojson/v2");
+const { pipeline } = require('stream');
 
 const sourcePath = "./csv/source.csv";
 const destPath = "./csv/out.txt";
 
 fs.writeFileSync(destPath, "");
 
-const readInterface = readline.createInterface({
-    input: fs.createReadStream(sourcePath),
-    console: false
-});
-
-let headers;
-
-readInterface.on('line', line => {
-    if (!headers) {
-        headers = line;
-        return;
+const csvToJsonOptions = {
+    output: 'json',
+    noheader: false,
+    delimiter: [","],
+    colParser: {
+        Book: "string",
+        Author: "string",
+        Amount: "omit",
+        Price: v => +v,
     }
+}
 
-    csv({
-        output: "json"
-    }).fromString(`${headers}\n` + line.toString()).then(json => {
-        const data = json[0];
+const readStream = fs.createReadStream(sourcePath);
+const writeStream = fs.createWriteStream(destPath);
 
-        const resLineData = {
-            book: data.Book,
-            author: data.Author,
-            price: +data.Price
-        };
+pipeline(
+    readStream,
+    csv(csvToJsonOptions),
+    writeStream,
+    err => err && console.log(err)
+);
 
-        fs.appendFile(destPath, JSON.stringify(resLineData) + "\n", err => { if (err) throw err; });
-    });
-})
